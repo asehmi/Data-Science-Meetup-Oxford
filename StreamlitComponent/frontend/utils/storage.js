6f8938dca51531bb08e2d8cc6a2aeb51a1ec8d6c
Arvindra Sehmi <vin@thesehmis.com>
2021-03-04 01:32:34 +0000

    renamed storage db

--- StreamlitComponent/frontend/utils/storage.js
+++ StreamlitComponent/frontend/utils/storage.js
@@ -7,8 +7,8 @@ import * as memoryDriver from 'localforage-driver-memory';
 localforage.defineDriver(memoryDriver)
 localforage.setDriver([localforage.LOCALSTORAGE, memoryDriver._driver, localforage.INDEXEDDB, localforage.WEBSQL])
 const STORAGE = localforage.createInstance({
-  name: 'scraper-app-0.1',
-  storeName: 'scraper-app-0.1'
+  name: 'streamlit-login-app-0.1',
+  storeName: 'streamlit-login-app-0.1'
 })
 
 export default STORAGE
